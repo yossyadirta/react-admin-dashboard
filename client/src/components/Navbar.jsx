1be2c956139e8592c4c9b2e7b0137612cd0b9176
Yossy Adirta <yossyadirta@gmail.com>
2023-04-06 16:05:51 +0700

    added sidebar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
+import React, { 
+  // useState
+ } from 'react';
 import { 
   LightModeOutlined, 
   DarkModeOutlined, 
   Menu as MenuIcon, 
   Search,
   SettingsOutlined,
-  ArrowDropDownOutlined 
+  // ArrowDropDownOutlined 
 } from "@mui/icons-material";
 import FlexBetween from './FlexBetween';
 import { useDispatch } from 'react-redux';
 import { setMode } from '../state';
-import profileImage from "../assets/profile.jpg";
+// import profileImage from "../assets/profile.jpg";
 import { useTheme } from '@emotion/react';
 import { AppBar, IconButton, InputBase, Toolbar } from '@mui/material';
 
 const Navbar = () => {
   const dispatch = useDispatch();
   const theme = useTheme();
-
-  console.log(theme, "<< ini theme");
 
   return (
     <AppBar
