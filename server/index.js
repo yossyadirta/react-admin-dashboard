0f51fd9a9fa14e8a343b06a0b1a7ae46cfcda803
Yossy Adirta <yossyadirta@gmail.com>
2023-04-08 18:18:03 +0700

    Overview page client and sever done

--- server/index.js
+++ server/index.js
@@ -15,13 +15,15 @@ import salesRoutes from "./routes/sales.js";
 import User from "./models/User.js";
 import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
+import OverallStat from "./models/OverallStat.js";
+import Transaction from "./models/Transaction.js";
 import {
   dataUser,
   dataProduct,
   dataProductStat,
   dataTransaction,
+  dataOverallStat,
 } from "./data/index.js";
-import Transaction from "./models/Transaction.js";
 
 /* configuration */
 dotenv.config();
@@ -55,6 +57,7 @@ mongoose
     // Product.insertMany(dataProduct);
     // ProductStat.insertMany(dataProductStat);
     // Transaction.insertMany(dataTransaction);
+    // OverallStat.insertMany(dataOverallStat);
   })
   .catch((error) => {
     console.log(error + "did not connect");
